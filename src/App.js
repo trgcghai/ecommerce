ec2a61c1e7e0ced79635bdffff5412f24edc3efa
trgcghai <conghai.tpma@gmail.com>
2023-11-02 20:25:07 +0700

    setup redux for react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import "./App.css";
+import { Counter } from "./features/counter/Counter";
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Counter />
+    </>
+  );
 }
